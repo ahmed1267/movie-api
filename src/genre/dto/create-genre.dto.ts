bea5b5132643ba75ebb739b1f28f0bf8dff9aca5
ahmed1267 <ahmedadel837@gmail.com>
2023-08-28 23:32:47 +0300

    Adjusted genre create DTO

--- src/genre/dto/create-genre.dto.ts
+++ src/genre/dto/create-genre.dto.ts
@@ -1,8 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator'
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
 
 // DTO class to create a new genre
 export class CreateGenreDto {
     @IsNotEmpty({ message: 'A genre must have a name' }) // Name is required
     @IsString({ message: 'A genre must have a string name' }) // Name must be a string
+    @MaxLength(15, { message: 'The max length for the genre name is 15' })
+    @MinLength(3, { message: 'The minimum length for the genre name is 3' })
     name: string // The name of the genre
 }
